ebd0f8152fba2cfe3035265fad477c0dc04c8390
Roger Sherman <rsherman@linux.com>
2022-10-26 20:09:59 +1000

    Added conditional for messaging and exit status if endpoint can't be reached, removed verbosity flag from nc command

--- endpoint_check.sh
+++ endpoint_check.sh
@@ -2,6 +2,8 @@
 input="list"
 while IFS=: read -r f1 f2 f3
 do
-        nc -zv -J 1 "$f2" "$f3"
-        echo "$f1"
+        if ! nc -z "$f2" "$f3";
+            then echo "$f1 can't be reached"
+            exit 1
+        fi
 done < "$input"
