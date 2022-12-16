94f7925e7af3aaae9b519ed32d8896898498084e
Roger Sherman <rsherman@linux.com>
2022-12-16 16:24:01 +1000

    Updated to pass failed connections to an array

--- endpoint_check.sh
+++ endpoint_check.sh
@@ -1,9 +1,21 @@
 #!/bin/bash
-input="list"
-while IFS=: read -r f1 f2 f3
-do
-        if ! nc -z "$f2" "$f3";
-            then echo "$f1 can't be reached"
-            exit 1
-        fi
-done < "$input"
+
+# Declare an array to store the failed connections
+failed_connections=()
+
+# Read in a list of endpoints and ports from a file (one endpoint and port per line, separated by a space)
+while read -r endpoint port; do
+  # Use nc to check if the endpoint is reachable
+  if nc -z "$endpoint" "$port"; then
+    echo "Connection to $endpoint on port $port succeeded"
+  else
+    # If the connection failed, add the endpoint to the failed connections array
+    failed_connections+=("$endpoint:$port")
+    echo "Connection to $endpoint on port $port failed"
+  fi
+done < endpoints.txt
+
+# If there were any failed connections, print them out
+if [ ${#failed_connections[@]} -gt 0 ]; then
+  echo "Failed connections: ${failed_connections[@]}"
+fi
